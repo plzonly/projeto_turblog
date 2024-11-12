534838ba9d98aa3e3d24b304a8902656cf09a21d
plzonly <pedro.jasmin@sptech.school>
2024-11-12 13:41:40 -0300

    Ajustando dashboard

--- web-data-viz/public/js/sessao.js
+++ web-data-viz/public/js/sessao.js
@@ -17,20 +17,3 @@ function limparSessao() {
     window.location = "../login.html";
 }
 
-// carregamento (loading)
-function aguardar() {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "flex";
-}
-
-function finalizarAguardar(texto) {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "none";
-
-    var divErrosLogin = document.getElementById("div_erros_login");
-    if (texto) {
-        divErrosLogin.style.display = "flex";
-        divErrosLogin.innerHTML = texto;
-    }
-}
-
