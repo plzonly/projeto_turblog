c71f2f9e9d3a7ca7469fb20388512fcb39c21ba8
plzonly <pedro.jasmin@sptech.school>
2025-01-07 23:23:30 -0300

    Criando variaveis e tabelas no SQL

--- web-data-viz/src/controllers/usuarioController.js
+++ web-data-viz/src/controllers/usuarioController.js
@@ -44,21 +44,30 @@ function autenticar(req, res) {
 
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+
+    //Criando variável 'carrofav'
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    var carrofav = req.body.carroServer;
 
     // Faça as validações dos valores
+
+    //Incrementando a variável 'carrofav'
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
+    } else if (carrofav == undefined) {
+        res.status(400).send("Seu carro favorito está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+
+        //Passando o valor do 'carrofav' como parâmetro
+        usuarioModel.cadastrar(nome, email, senha, carrofav)
             .then(
                 function (resultado) {
                     res.json(resultado);
--- web-data-viz/src/models/usuarioModel.js
+++ web-data-viz/src/models/usuarioModel.js
@@ -10,15 +10,26 @@ function autenticar(email, senha) {
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function cadastrar(nome, email, senha) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
+
+//Adicionando a variavel 'carrofav'
+function cadastrar(nome, email, senha, carrofav) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, carrofav);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
         INSERT INTO usuario (nome, email, senha) VALUES ('${nome}', '${email}', '${senha}');
     `;
+
+    //Adicionando a variavel 'inserindoCarroFavorito' para inserir o carro escolhido pelo usuario no SQL
+    var inserindoCarroFavorito = `
+        INSERT INTO carro (nome) VALUES ('${carrofav}');   
+    `
+
+    //Adicionando o console.log para saber em qual etapa está e 'database.executar' para executar a função criada
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    console.log("Inserindo carro no SQL: \n" + inserindoCarroFavorito);
+    database.executar(inserindoCarroFavorito);
     return database.executar(instrucaoSql);
 }
 
